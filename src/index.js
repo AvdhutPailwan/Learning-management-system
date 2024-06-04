3a6d625359c30222b52a34f2a504b0d05eea4a24
Avdhut Pailwan <avdhutpailwan@gmail.com>
2024-06-04 18:11:25 +0530

    Added lint-staged, prettier and eslint
    Made some changes in configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-undef */
+import { describe, test, expect } from "jest";
+
 describe(`sample test suite`, () => {
   test(`sample test case`, () => {
     expect(true).toBe(true);
